c54aea12ddfee0fdf74f0ef606dce4e1620445ec
Sujil Anto Thomas <Sujilnt@gmail.com>
2022-01-15 20:33:59 +0000

    adding some stylea insert in the html page

--- project/src/server/apod/index.mjs
+++ project/src/server/apod/index.mjs
@@ -11,11 +11,10 @@ console.log("key",process.cwd(), '.env');
 const url =`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`;
 
 const getImages = async (req,res)=>{
-    console.log("request",res,req)
     try {
-        let response = await fetch(url);
-        const image = await response.json();
-        res.send({image})
+        const response = await fetch(url);
+        const data = await response.json();
+        res.status(201).json(data);
     } catch (err) {
         console.log('error:', err);
     }
